e3f52d86de420f3703bef70e1f3dfd489ac3ed7e
Itamar Tati <itamar.tati1994@gmail.com>
2020-12-18 05:11:46 +0000

    created header and styled header

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import Table from '../../components/table/Table'
+import Table from '../../components/table/Table';
+import classes from './Home.module.css';
+
 
 const Home: React.FC = () => {
     return (
     <div >
-        <h1>HomePage</h1>
-
+      <div className={classes.Header}>
+          <h2>Welcome to </h2>
+          <h3>Because behind every scan is a person. </h3>
+      </div>
         <Table/>
     
 
