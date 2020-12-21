f5c3bc42dc25de6034a7b78a28f79c82233b12e2
Itamar Tati <itamar.tati1994@gmail.com>
2020-12-21 05:58:45 +0000

    fixed bug in shortenddate function

--- src/functions/shortendDate.test.ts
+++ src/functions/shortendDate.test.ts
@@ -2,4 +2,8 @@ import shortendDate from './shortendDate';
 test('Take a date code and convert it into a readable date', () => {
   expect(shortendDate('2005-09-09T00:00:00.000Z')).toBe('9th of September, 2005');
   expect(shortendDate('2008-03-24T00:00:00.000Z')).toBe('24th of March, 2008');
+  expect(shortendDate('2005-09-01T00:00:00.000Z')).toBe('1st of September, 2005');
+  expect(shortendDate('2008-03-23T00:00:00.000Z')).toBe('23rd of March, 2008');
+  expect(shortendDate('2005-09-02T00:00:00.000Z')).toBe('2nd of September, 2005');
+  expect(shortendDate('2008-03-22T00:00:00.000Z')).toBe('22nd of March, 2008');
 });
--- src/functions/shortendDate.ts
+++ src/functions/shortendDate.ts
@@ -1,14 +1,22 @@
 export default function shortendDate(date: string): string {
-    const Months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    let daySeperationFromTime = date.split('T')
-    let dateSplit = daySeperationFromTime[0].split('-')
-    switch (dateSplit[2].charAt(dateSplit[2].length - 1)) {
-        case '1':
+    const Months: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    let daySeperationFromTime: string[] = date.split('T')
+    let dateSplit: string[] = daySeperationFromTime[0].split('-')
+    switch (dateSplit[2]) {
+        case '01':
             return `${Number(dateSplit[2])}st of ${Months[Number(dateSplit[1]) - 1]}, ${dateSplit[0]}`
-        case '2':
+        case '02':
             return `${Number(dateSplit[2])}nd of ${Months[Number(dateSplit[1]) - 1]}, ${dateSplit[0]}`
-        case '3':
+        case '03':
             return `${Number(dateSplit[2])}rd of ${Months[Number(dateSplit[1]) - 1]}, ${dateSplit[0]}`
+        case '21':
+            return `${Number(dateSplit[2])}st of ${Months[Number(dateSplit[1]) - 1]}, ${dateSplit[0]}`
+        case '22':
+            return `${Number(dateSplit[2])}nd of ${Months[Number(dateSplit[1]) - 1]}, ${dateSplit[0]}`
+        case '23':
+            return `${Number(dateSplit[2])}rd of ${Months[Number(dateSplit[1]) - 1]}, ${dateSplit[0]}`
+        case '31':
+            return `${Number(dateSplit[2])}st of ${Months[Number(dateSplit[1]) - 1]}, ${dateSplit[0]}`
         default:
             return `${Number(dateSplit[2])}th of ${Months[Number(dateSplit[1]) - 1]}, ${dateSplit[0]}`
     }
